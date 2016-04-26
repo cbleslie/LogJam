928da0502d4f6d0d48b4050c3c4c1d99d4ee63c4
Cameron Leslie <cameronleslie@gmail.com>
2016-04-26 10:16:36 -0400

    just getting this stuff turned on

---
+++ Gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+//calling this seperately, 'cause .create.
+var browserSync = require('browser-sync').create();
+
+var paths = {
+  dist: 'dist/',
+  src: 'src/',
+};
+
+
+gulp.task('styles', function () {
+  gulp.src(paths.src + '**/*.scss')
+    .pipe(plugins.sass().on('error', plugins.sass.logError))
+    .pipe(plugins.autoprefixer({
+      browsers: ['IE 10','last 4 versions'],
+      cascade: true
+    }))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', function () {
+  gulp.src(paths.src + 'index.html')
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('default', ['html','styles'], function() {
+  browserSync.init({
+      server: paths.dist
+  });
+
+  gulp.watch(paths.src + "*.scss", ['styles']);
+  gulp.watch(paths.src + "*.html", ['html']).on('change', browserSync.reload);
+});
